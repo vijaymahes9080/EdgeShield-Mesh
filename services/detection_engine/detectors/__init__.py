dee4d55b35f3bbdd029cf9a552d7f74ceb6b0b17
Vijay Mahes <Vijaypradhap2004@gmail.com>
2026-09-06 12:51:40 +0530

    refactor(detection): export DeterministicDetectors and all advanced detectors from detectors package

--- services/detection_engine/detectors/__init__.py
+++ services/detection_engine/detectors/__init__.py
@@ -1,1 +1,17 @@
 """Detector package initialization."""
+
+from services.detection_engine.deterministic import DeterministicDetectors
+from services.detection_engine.detectors.neuromorphic_spiking import LeakyIntegrateFireDetector
+from services.detection_engine.detectors.isolation_forest import StreamingIsolationForestDetector
+from services.detection_engine.detectors.acoustic_side_channel import AcousticSideChannelDetector
+from services.detection_engine.detectors.geo_spatial_teleportation import GeoSpatialTeleportationDetector
+from services.detection_engine.detectors.quantum_entropy_auditor import QuantumEntropyAuditor
+
+__all__ = [
+    "DeterministicDetectors",
+    "LeakyIntegrateFireDetector",
+    "StreamingIsolationForestDetector",
+    "AcousticSideChannelDetector",
+    "GeoSpatialTeleportationDetector",
+    "QuantumEntropyAuditor"
+]
